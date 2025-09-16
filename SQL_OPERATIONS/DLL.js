239429ad0951e6e3bc3c4b008337207b750a55a6
sarrthak008 <sarthaknavale08@gmail.com>
2025-09-16 13:18:13 +0530

    add a code for use database query

--- SQL_OPERATIONS/DLL.js
+++ SQL_OPERATIONS/DLL.js
@@ -1,21 +1,40 @@
 import connection from "../config/conectdb.js";
 
-const runDDL = async(req,res)=>{
-     try {
-        
-         // traying to create a new table of testing 
-         let responce = await connection.query("CREATE DATABASE IF NOT EXISTS ddl");
-           
-             if(responce.length == 0){
-                 return res.status(400).json({data:null,message:"database creation faild"});
-             }
+const runDDL = async (req, res) => {
+    try {
 
-         return res.status(200).json({data:responce})
+        // traying to create a new table of testing 
+        let responce = await connection.query("CREATE DATABASE IF NOT EXISTS ddl");
 
-     } catch (error) {
+        if (responce.length == 0) {
+            return res.status(400).json({ data: null, message: "database creation faild" });
+        } else {
+            console.log('database created successfully')
+        }
+
+
+
+        // use create dabase ...
+        let isDatabseUsed = await connection.query("USE ddl");
+
+        if (isDatabseUsed.length == 0) {
+            return res.status(400).json({ data: null, message: "faild to use database" });
+        } else {
+            console.log('database used successfully')
+        }
+
+
+
+
+
+
+
+        return res.status(200).json({ data: responce })
+
+    } catch (error) {
         console.log(error);
-        return res.status(error.status).json({data:null,message:error.message})
-     }
+        return res.status(error.status).json({ data: null, message: error.message })
+    }
 }
 
 export default runDDL;
